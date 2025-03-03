91aad89dd7120241cb581b8abfc3c861ce5e39ae
Guna4kaa <Guna4ka@mail.ru>
2025-03-04 01:01:47 +0500

    first commit

---
+++ lesson-1/script.js
@@ -0,0 +1,13 @@
+let persone = 
+{
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+console.log(persone["name"]);
+
+
+let arr = ['plum.png', 'apple.jpg', 'orange.bmp']
+console.log(arr[0, 2]);
+
