149f8a24c39454ebed5a32626f041a6b53387e07
Guna4kaa <Guna4ka@mail.ru>
2025-05-10 21:36:41 +0500

    Add 7hw Petrichenko

---
+++ Задания_Петриченко/Задание-7/js/script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('start');
+console.log(button);
+
+const blocks = document.querySelectorAll('[class$=-value]');
+console.log(blocks);
+
+const mandatoryExp = document.querySelectorAll('.expenses-item');
+console.log(mandatoryExp);
+
+const aprButton = document.getElementsByTagName('button');
+console.log(aprButton);
+
+const noMandatory = document.querySelectorAll('.optionalexpenses-item');
+console.log(noMandatory);
+
